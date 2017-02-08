2c105129e74894aba3357a902f635f0aa724a0f4
irfanhasan <ihasan03@gmail.com>
2017-02-07 23:37:42 -0600

    changed repOK to detect DAGs using topological sort

--- DAG/src/main/java/DAG/DAG.java
+++ DAG/src/main/java/DAG/DAG.java
@@ -9,31 +9,86 @@ public class DAG {
 	}
 	
 	private Node root;
+	//private Node[] nodes;
 	private int size;
 	
+	
+	
+
+	
 	public boolean repOK() {
+		//topological sort
 		if (root == null)
 			return size == 0;
-		
-		HashSet<Node> visited = new HashSet<Node>();
-		LinkedList<Node> toVisit = new LinkedList<Node>();
-		
-	
-		toVisit.add(root);
-		
-		while(!toVisit.isEmpty()) {
-			Node current = toVisit.remove();
-			visited.add(current); //adding current node to list of visited nodes
-			for (int i = 0; i < current.children.length; i++) {
-				Node c = current.children[i];
-				//check if we have already visited this node
-				if(visited.contains(c)) return false;
-				toVisit.add(c);
+		Node[] nodes = getList();
+		HashMap<Node, Integer> incoming = new HashMap<Node, Integer>();
+		//detecting all incoming edges
+		for (int i = 0; i < nodes.length; i++) {
+			Node current = nodes[i];
+			for (int j = 0; j < current.children.length; j++) {
+				Node child = current.children[j];
+				updateIncoming(child, true, incoming);
 			}
 		}
-		return visited.size() == size;
+		
+		incoming.put(root, 0);
+		
+		LinkedList<Node> noIncoming = new LinkedList<Node>();
+		LinkedList<Node> topSort = new LinkedList<Node>();
+		findNoIncoming(noIncoming, incoming);
+		while(!noIncoming.isEmpty()) {
+			Node current = noIncoming.remove();
+			topSort.add(current);
+			for (int i = 0; i < current.children.length; i++) {
+				Node child = current.children[i];
+				updateIncoming(child, false, incoming);
+				if (incoming.get(child) == 0) noIncoming.add(child);
+			}
+		}
+		return verifyDAG(incoming) && topSort.size() == size;
+		
 	}
 	
+	private void updateIncoming(Node n, boolean add, HashMap<Node, Integer> m) {
+		if (m.containsKey(n)) {
+			int update = add ? m.get(n) + 1 : m.get(n) - 1;
+			m.put(n, update);
+		} else {
+			m.put(n, 1);
+		}
+	}
+	private Node[] getList() {
+		ArrayList<Node> nodes = new ArrayList<Node>();
+		Queue<Node> queue = new LinkedList<Node>();
+		HashSet<Node> visited = new HashSet<Node>();
+		queue.add(root);
+		nodes.add(root);
+		visited.add(root);
+		while(!queue.isEmpty()) {
+			Node node = queue.remove();
+			for (int i = 0; i < node.children.length; i++) {
+				Node child = node.children[i];
+				if (!visited.contains(child)) {
+					queue.add(child);
+					visited.add(child);
+					nodes.add(child);
+				}
+			}
+		}
+		return nodes.toArray(new Node[nodes.size()]);
+	}
+	private boolean verifyDAG(HashMap<Node, Integer> m) {
+		for (Node n : m.keySet()) {
+			if (m.get(n) != 0) return false;
+		}
+		return true;
+	}
+	
+	private void findNoIncoming(LinkedList<Node> set, HashMap<Node, Integer> m) {
+		for (Node n : m.keySet()) {
+			if (m.get(n) == 0) set.add(n);
+		}
+	}
 	
 
 	
@@ -44,7 +99,7 @@ public class DAG {
 
         IObjSet nodes = f.createObjSet(Node.class, nodeNum, false);
         f.set("root", nodes);
-        
+		
         IIntSet arrLen = f.createIntSet(0, nodeNum - 1);
         IArraySet childrenArray = f.createArraySet(Node[].class, arrLen, nodes, nodeNum);
         
